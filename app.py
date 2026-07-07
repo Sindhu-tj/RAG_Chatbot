d72efa8f33586424b6e7dbf7e732bc6803da2c0d
YOUR_NAME <sindhuaradhya97@gmail.com>
2026-07-07 22:22:41 +0530

    Updated website content

--- app.py
+++ app.py
@@ -94,12 +94,10 @@ def load_ocr():
 def load_model():
 
     pipe = pipeline(
-        "text2text-generation",
-        model="google/flan-t5-base",
-        tokenizer="google/flan-t5-base",
-        max_new_tokens=200,
-        do_sample=False
-    )
+    "text-generation",
+    model="gpt2",
+    max_new_tokens=200
+)
 
     return pipe
 
@@ -608,5 +606,5 @@ if uploaded_file:
 else:
 
     st.info(
-        "📂 Upload a file to begin."
+         "📂 Upload a file to begin."
     )
